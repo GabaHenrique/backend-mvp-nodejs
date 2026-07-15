f0e617522234fc5885338ed0b7fbabe2da082817
Gabriel Henrique <145069184+GabaHenrique@users.noreply.github.com>
2026-07-15 15:01:21 -0300

    Fix: Redis URL

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,8 +1,10 @@
 const Redis = require('ioredis');
 
-const redisClient = new Redis({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT
-});
+const redisClient = process.env.REDIS_URL
+  ? new Redis(process.env.REDIS_URL)
+  : new Redis({ host: process.env.REDIS_HOST, port: process.env.REDIS_PORT });
 
 module.exports = redisClient;
+
+
+
