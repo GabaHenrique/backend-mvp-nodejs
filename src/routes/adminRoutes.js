6210130d52ddbe08e883201edbbb059983952aed
Gabriel Henrique <145069184+GabaHenrique@users.noreply.github.com>
2026-07-15 11:25:50 -0300

    Implementando Redis, rate-limit no adminlogin

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/adminController');
-const authMiddleware = require('../middlewares/authMiddleware'); 
+const authMiddleware = require('../middlewares/authMiddleware');
+const rateLimit = require('express-rate-limit');
+const { RedisStore}  = require('rate-limit-redis');
+const redisClient = require('../config/redis');
+
+const loginLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  message: { error: 'Muitas tentativas de login. Tente novamente em 15 minutos.' },
+  store: new RedisStore({
+    sendCommand: (...args) => redisClient.call(...args)
+  })
+});
 
 router.get('/dashboard', authMiddleware, adminController.getDashboard);
 router.post('/register', authMiddleware, adminController.register);
-router.post('/login', adminController.login);
+router.post('/login', loginLimiter, adminController.login);
 
 module.exports = router;
